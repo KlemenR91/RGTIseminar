15820e5a7d3b27408a4ae0ed189c31b22f2f7d3a
matkladnik <matic.kladnik@gmail.com>
2014-11-20 00:50:00 +0100

    Narejen osnovni nadzor za premikanje, osnovna kamera (je pripojena na objekt), narejeno ozadje.

--- js/spacesmuggler.js
+++ js/spacesmuggler.js
@@ -1,24 +1,68 @@
 
-var MAX_SPEED = 5.0;
+var MAX_SPEED = 1.0;
+var MIN_SPEED = -1.0;
+var TURN_FACTOR = 0.05;
 
-//setting up scene
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var playerObject;
+var scene;
+var camera;
+var renderer;
+var backgroundPlane;
+var backgroundTexture;
+var backgroundMaterial
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight);
-//renderer.setSize( 960, 540);
-document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x0021f0 } );
+function initialize() {
+	//setting up scene
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight);
+	//renderer.setSize( 960, 540);
+	document.body.appendChild( renderer.domElement );
+	
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { color: 0x0021f0 } );
+	
+	//var cube = new THREE.Mesh( geometry, material );
+	//scene.add( cube );
+	playerObject = new THREE.Mesh( geometry, material );
+	scene.add( playerObject );
+	
+	var backgroundFilePath = "res/free_space_galaxy_texture.jpg";
+	setBackground(backgroundFilePath);
+	
+	playerObject.add(camera);		// za TEST - potrebna izboljsava
+	camera.position.z = 15;
+}
 
-//var cube = new THREE.Mesh( geometry, material );
-//scene.add( cube );
-var playerObject = new THREE.Mesh( geometry, material );
-scene.add( playerObject );
+function setBackground(path) {
+	//space texture (background)   - BACKGROUND
+	THREE.ImageUtils.crossOrigin = '';
+	backgroundTexture = THREE.ImageUtils.loadTexture(path);
+	// assuming you want the texture to repeat in both directions:
+	backgroundTexture.wrapS = THREE.RepeatWrapping; 
+	backgroundTexture.wrapT = THREE.RepeatWrapping;
+	// how many times to repeat in each direction; the default is (1,1),
+	//   which is probably why your example wasn't working
+	//backgroundTexture.repeat.set( 4, 4 ); 
+	
+	backgroundMaterial = new THREE.MeshBasicMaterial({ map : backgroundTexture });
+	backgroundPlane = new THREE.Mesh(new THREE.PlaneGeometry(160, 90), backgroundMaterial);
+	//backgroundPlane.material.side = THREE.DoubleSide;
+	//backgroundPlane.position.x = 10;
+	//backgroundPlane.position.y += 10;
+	backgroundPlane.position.z -= 10;
 
-camera.position.z = 5;
+	//backgroundPlane.rotation.z = Math.PI / 2;
+	scene.add(backgroundPlane);
+	// END OF SETTING BACKGROUND
+	
+}
+
+initialize();
+
 
 //map for input values
 var activeKeys = {};
@@ -75,23 +119,26 @@ function handleKeyUp(event) {
 
 function handleKeys() {
 	
+	//rotation
 	if (activeKeys["rotateLeft"] == 1) {
 		// Left cursor key
-		//rotationVelocityCubeY -= 1;
-		//alert("yay");
-	}
-	if (activeKeys["rotateRight"] == 1) {
+		turn = 1 * TURN_FACTOR;
+	} else if (activeKeys["rotateRight"] == 1) {
 		// Right cursor key
-		//rotationVelocityCubeY += 1;
+		turn = -1 * TURN_FACTOR;
+	} else {
+		turn = 0;
 	}
+	
+	//movement
 	if (activeKeys["forward"] == 1) {
 		// Up cursor key
-		//rotationVelocityCubeX -= 1;
-		speed = 1;								//TEST
-	}
-	if (activeKeys["backward"] == 1) {
+		speed = 0.4;								//TEST
+	} else if (activeKeys["backward"] == 1) {
 		// Down cursor key
-		//rotationVelocityCubeX += 1;
+		speed = -0.4;
+	} else {
+		speed = 0;
 	}
 }
 
@@ -103,7 +150,7 @@ function resetMovementVars() {
 	
 	moveForward = 0.0;
 	moveBackward = 0.0;
-	turn = 0.0;
+	//turn = 0.0;
 }
 
 function moveAndRotate() {
@@ -111,8 +158,8 @@ function moveAndRotate() {
 		speed = MAX_SPEED;
 	}
 	
-	if(speed < 0) {
-		speed = 0.0;
+	if(speed < MIN_SPEED) {
+		speed = MIN_SPEED;
 	}
 	
 	//moveForward += speed;
@@ -120,7 +167,9 @@ function moveAndRotate() {
 
 	//playerObject.rotation.z += rotate;
 	//playerObject.position
-	playerObject.translateY(0.01);
+	playerObject.translateY(speed);
+	
+	playerObject.rotation.z += turn;
 }
 
 document.onkeydown = handleKeyDown;
@@ -138,6 +187,10 @@ var render = function () {
 	resetMovementVars();
 	moveAndRotate();
 
+	//changing the camera position a bit (so its not on top of the object
+	camera.position.x = 0;
+	camera.position.y = 6;
+	
 	renderer.render(scene, camera);
 };
 
